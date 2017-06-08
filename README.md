95fdfe5db9e2ebd9497f77f5ad76a33e10e04aa4
Yann <vernetty@tice-ead.upmf-grenoble.fr>
2017-06-08 13:49:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# moodle-enrol_candidat
+A template for Moodle Enrol plugins based on enrol/self to be used with Moosh plugin generation
---
+++ db/access.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Capabilities for candidat enrolment plugin.
+ *
+ * @package    enrol_candidat
+ * @copyright  2010 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    /* Add or edit enrol-candidat instance in course. */
+    'enrol/candidat:config' => array(
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+
+    /* Manage user candidat-enrolments. */
+    'enrol/candidat:manage' => array(
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+
+    'enrol/candidat:holdkey' => array(
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'manager' => CAP_PROHIBIT,
+        )
+    ),
+
+    /* Voluntarily unenrol candidat from course - watch out for data loss. */
+    'enrol/candidat:unenrolcandidat' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'student' => CAP_ALLOW,
+        )
+    ),
+
+    /* Unenrol anybody from course (including candidat) -  watch out for data loss. */
+    'enrol/candidat:unenrol' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+
+);
